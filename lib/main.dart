4b6140bb2c02641f927f18902a6d4f7052679642
Dragon708 <jorgegmdgonzalez@gmail.com>
2022-09-17 17:04:41 -0500

    ok

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'Pages/Inicio.dart';
+import 'Pages/Principales/CatalogoShop.dart';
+import 'Pages/Principales/Servicios.dart';
+import 'Pages/Principales/Ubicacion.dart';
+import 'Pages/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'El Paraiso Perruno',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          '/': (context) => const Homepage(),
+          'inicio': (context) => const Inicio(),
+          "Catalogo": (context) => Catalogo(),
+          "servicios": (context) => Servicios(),
+          "ubicacion": (context) => Ubicacion(),
+        });
+  }
+}
